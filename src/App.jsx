604519a5827006c15ca1ca4fabac74b4827ea916
Carlos Nieto <99897631+FlyerBird@users.noreply.github.com>
2023-10-16 00:35:22 +0200

    Styles1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import UserForm from './components/UserForm';
 import SoldPets from './components/SoldPets';
 import SameNamePets from './components/SameNamePets';
+import './App.css'
 
 function App() {
   return (
